8cab4f9d4172d80d7f211d4c3960eb8b4554d02c
Dung Doan <doananhdung.work@gmail.com>
2026-08-07 09:55:08 +0700

    fix: publish public WebRTC ICE candidates

--- deploy/deploy-remote.sh
+++ deploy/deploy-remote.sh
@@ -20,9 +20,37 @@ sudo install -m 0644 deploy/systemd/simlydent.service /etc/systemd/system/simlyd
 sudo install -m 0644 deploy/systemd/freeswitch.service /etc/systemd/system/freeswitch.service
 
 sudo install -m 0644 deploy/freeswitch/conf/sip_profiles/zcc.xml "$FS_CONF/sip_profiles/zcc.xml"
+sudo install -d "$FS_CONF/directory/default"
+sudo install -m 0644 deploy/freeswitch/conf/directory/default/1001.xml "$FS_CONF/directory/default/1001.xml"
 sudo install -m 0644 deploy/freeswitch/conf/dialplan/default/05_zcc_inbound.xml "$FS_CONF/dialplan/default/05_zcc_inbound.xml"
 sudo install -m 0644 deploy/freeswitch/conf/dialplan/default/10_zcc_outbound.xml "$FS_CONF/dialplan/default/10_zcc_outbound.xml"
 sudo install -m 0644 deploy/freeswitch/conf/dialplan/zcc.xml "$FS_CONF/dialplan/zcc.xml"
+
+# Keep the vanilla internal profile, but make its WebRTC ICE candidate public.
+# The browser connects from the Internet; advertising the EC2 private address
+# (172.31.x.x) causes it to send RTP to an unroutable candidate and hang up
+# immediately after ACK.
+INTERNAL_PROFILE="$FS_CONF/sip_profiles/internal.xml"
+SWITCH_CONF="$FS_CONF/autoload_configs/switch.conf.xml"
+if [ -f "$INTERNAL_PROFILE" ]; then
+  sudo sed -i \
+    's|<param name="ext-rtp-ip"[^>]*/>|<param name="ext-rtp-ip" value="stun:stun.freeswitch.org"/>|' \
+    "$INTERNAL_PROFILE"
+  if ! sudo grep -q 'name="stun-enabled"' "$INTERNAL_PROFILE"; then
+    sudo sed -i '/<param name="ws-binding"/i\    <param name="stun-enabled" value="true"/>' "$INTERNAL_PROFILE"
+    sudo sed -i '/<param name="ws-binding"/i\    <param name="stun-auto-disable" value="false"/>' "$INTERNAL_PROFILE"
+  fi
+fi
+
+# Keep the AWS security-group RTP range in sync with FreeSWITCH.
+if [ -f "$SWITCH_CONF" ]; then
+  sudo sed -i \
+    's|<!--[[:space:]]*<param name="rtp-start-port" value="[0-9]*"/>[[:space:]]*-->|<param name="rtp-start-port" value="10000"/>|' \
+    "$SWITCH_CONF"
+  sudo sed -i \
+    's|<!--[[:space:]]*<param name="rtp-end-port" value="[0-9]*"/>[[:space:]]*-->|<param name="rtp-end-port" value="20000"/>|' \
+    "$SWITCH_CONF"
+fi
 
 sudo chown -R simlydent:simlydent "$APP_DIR"
 sudo chown -R freeswitch:freeswitch "$FS_DIR/var/lib/freeswitch/recordings"
